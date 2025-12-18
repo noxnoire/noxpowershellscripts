5a0c0659a5beef57c2d777bb83834dbdaa85af15
noxnoire <noxnoire@gmail.com>
2025-12-18 12:16:44 -0300

    Enhance exeswap.ps1 with output for paths
    Added output statements for original and final paths.

--- exeswap.ps1
+++ exeswap.ps1
@@ -2,4 +2,6 @@ $originalString = $args[0]
 $originalExe = $args[1]
 $finalExe = $args[2]
 $finalPath = $originalString.Replace($originalExe, $finalExe)
+Write-Output $originalString
+Write-Output $finalPath
 Start-Process $finalPath
