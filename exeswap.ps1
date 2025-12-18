54f1a0fa363317b6bd6d081d73458e21cca95a9c
noxnoire <noxnoire@gmail.com>
2025-12-18 13:05:00 -0300

    Update exeswap.ps1

--- exeswap.ps1
+++ exeswap.ps1
@@ -1,4 +1,4 @@
-param([string]$OriginalPath [string]$replace [string]$replacement)
+param([string]$OriginalPath,[string]$replace,[string]$replacement)
 Write-Host $OriginalPath
 Write-Host $replace
 Write-Host $replacement
